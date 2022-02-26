43c406e643b871d0dc62f0960653860fc3b03d60
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-26 16:53:58 +0000

    Returning data  to generate using template literal

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,2 +1,14 @@
 // function generating markdown for README File
 function generateMarkdown(data) {
+  // The data requested by the inquirer is returned. Using that data to personalise a markdown page by writing inside the back ticks.
+  return `# ${data.title}
+  ----
+  
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Test Instructions](#test-instructions)
+  - [Contact Information](#questions?)
+
