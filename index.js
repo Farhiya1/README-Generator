01384441c384d82ac49255320d03d293f50cddf2
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-26 16:46:37 +0000

    Packages needed and file paths

---
+++ index.js
@@ -0,0 +1,5 @@
+// Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+const fileName = "./output/README.md";
