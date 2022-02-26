7d65f09d0099244767fb4444f44bd3a92ded4ebb
Farhiya Mahamud <farhiya.i.mahamud@gmail.com>
2022-02-26 16:47:22 +0000

    Array of questions:title

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown.js");
 const fileName = "./output/README.md";
+
+// Array of questions for users
+const questions = [
+  {
+    type: "input",
+    message: "What is the title of your project?",
+    name: "title",
+  },
